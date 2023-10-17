94533b3f8d73e6fd044a997db3287eece08ffbaa
kris.wang <wenhom.wang@gmail.com>
2023-10-18 01:17:14 +0800

    update result info

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ virtualenv env
 source ./env/bin/activate
 pip install -r requirements.txt
 ```
+
+## Example
+
+``` bash
+>> python3 .\test_ellipsoid_fit.py
+
+    Ground Truth
+    -- sens --------------------------- bias ------- radi
+     0.95000,  0.02000,  0.04000,     -10.000,      35.000,
+     0.02000,  1.05000,  0.06000,      20.000,
+     0.04000,  0.06000,  1.00000,     -30.000,
+    
+    Ellipsoid Fitting
+    -- sens --------------------------- bias ------- radi
+     0.95252,  0.02005,  0.04011,     -10.000,      35.093,
+     0.02005,  1.05278,  0.06016,      20.000,
+     0.04011,  0.06016,  1.00265,     -30.000,
+```
+
+![](figure/plot.png)
--- test_ellipsoid_fit.py
+++ test_ellipsoid_fit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 import fittinglib as ft
 
@@ -7,6 +8,13 @@ import fittinglib as ft
 p_radi = 35
 p_sens = np.array([[0.95, 0.02, 0.04], [0.02, 1.05, 0.06], [0.04, 0.06, 1.0]])
 p_bias = np.array([-10,20,-30]).reshape(3,1)
+
+print(f'''
+    Ground Truth
+    -- sens --------------------------- bias ------- radi
+    {p_sens[0,0]:8.5f}, {p_sens[0,1]:8.5f}, {p_sens[0,2]:8.5f},   {p_bias[0,0]:9.3f},   {p_radi:9.3f},
+    {p_sens[1,0]:8.5f}, {p_sens[1,1]:8.5f}, {p_sens[1,2]:8.5f},   {p_bias[1,0]:9.3f},
+    {p_sens[2,0]:8.5f}, {p_sens[2,1]:8.5f}, {p_sens[2,2]:8.5f},   {p_bias[2,0]:9.3f},''')
 
 v = p_radi * ft.get_sphere(n=300)
 vr = (np.linalg.inv(p_sens) @ v + p_bias)
@@ -18,20 +26,52 @@ bias = fit.get('bias')
 radi = fit.get('radi')
 vc = (sens @ (vr - bias))
 
-print(sens)
-print(bias)
-print(radi)
+print(f'''
+    Ellipsoid Fitting
+    -- sens --------------------------- bias ------- radi
+    {sens[0,0]:8.5f}, {sens[0,1]:8.5f}, {sens[0,2]:8.5f},   {bias[0,0]:9.3f},   {radi:9.3f},
+    {sens[1,0]:8.5f}, {sens[1,1]:8.5f}, {sens[1,2]:8.5f},   {bias[1,0]:9.3f},
+    {sens[2,0]:8.5f}, {sens[2,1]:8.5f}, {sens[2,2]:8.5f},   {bias[2,0]:9.3f},
+''')
 
 # %%
 
 x, y, z = v
-fig = plt.figure("sphere points")
-ax = fig.add_subplot(111, aspect='equal', projection='3d')
+fig = plt.figure("ellipsoid fitting", tight_layout=True, figsize=(12, 8))
+gs = gridspec.GridSpec(3, 4, figure=fig)
 
-# ax.plot_surface(x, y, z, cmap=plt.cm.YlGnBu_r)
-ax.scatter(*vr)
-ax.scatter(*vc)
-ax.axis('equal')
+ax1 = fig.add_subplot(gs[0, 0])
+ax1.plot(vr[0, :], vr[1, :], '.')
+ax1.plot(vc[0, :], vc[1, :], '.')
+ax1.axis('equal')
+ax1.grid()
+ax1.set_xlabel('X-axis [uT]')
+ax1.set_ylabel('Y-axis [uT]')
+
+ax2 = fig.add_subplot(gs[1, 0], aspect='equal')
+ax2.plot(vr[1, :], vr[2, :], '.')
+ax2.plot(vc[1, :], vc[2, :], '.')
+ax2.axis('equal')
+ax2.grid()
+ax2.set_xlabel('Y-axis [uT]')
+ax2.set_ylabel('Z-axis [uT]')
+
+ax3 = fig.add_subplot(gs[2, 0], aspect='equal')
+ax3.plot(vr[2, :], vr[0, :], '.')
+ax3.plot(vc[2, :], vc[0, :], '.')
+ax3.axis('equal')
+ax3.grid()
+ax3.set_xlabel('Z-axis [uT]')
+ax3.set_ylabel('X-axis [uT]')
+
+ax3d = fig.add_subplot(gs[:, 1:], aspect='equal', projection='3d')
+ax3d.scatter(*vr)
+ax3d.scatter(*vc)
+ax3d.axis('equal')
+ax3d.set_xlabel('X-axis [uT]')
+ax3d.set_ylabel('Y-axis [uT]')
+ax3d.set_zlabel('Z-axis [uT]')
+
 plt.show()
 
 # %%
